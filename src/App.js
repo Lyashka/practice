24e411f0917dd607b946fa957f1c06a3da397fe4
ILa <merzlyaha@mail.ru>
2022-07-20 01:20:25 +0300

    function commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
 import './App.css';
+import Sign from './components/sign/sign';
+import RegPartner from './components/RegPartner/RegPartner';
+import Registration from './components/registration/registration';
+import RegSubdivision from './components/regSubdivision/regSubdivision';
+import MainMenu from './components/mainMenu/mainMenu';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Sign/>}/>
+          <Route path='/registration' element={<Registration/>}/>
+          <Route path='/partner' element={<RegPartner/>}/>
+          <Route path='/subdivision' element={<RegSubdivision/>}/>
+          <Route path='/mainmenu' element={<MainMenu/>}/>
+        </Routes>
+      </BrowserRouter>
+    
     </div>
   );
 }
