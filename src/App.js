68ef86ae83f90e8b8bd1b755c3ce3b74c1443392
ILa <merzlyaha@mail.ru>
2022-09-01 06:38:11 +0300

    new components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,26 @@ import {BrowserRouter, Route, Routes, HashRouter} from 'react-router-dom';
 function App() {
 
   const data = [
-    {"Email": "i1a@mail.ru", "password": "123456" , "name": "IlyaMerzlyakov", "kategory": "partner"},
+    {
+      "Email": "i1a@mail.ru", 
+      "password": "123456" , 
+      "name": "IlyaMerzlyakov", 
+      "kategory": "partner", 
+      "partners": [
+                    {
+                      "namePartner": ["name1", 'name2'],
+                      "typeAgreement": "Обоюдное",
+                      "dateStart": "11.01.11",
+                      "dateEnd": "11.11.11",
+                      "statusAgreement": "Active",
+                      "contactsPartner": "88005553535",
+                      "divisions": ["bla1", "bla2"]
+                    },
+                    {
+                      "namePartner": ["name"]
+                    }
+                  ]  
+      },
     {"Email": "1@mail.ru", "password": "123456" , "name": "Lopuh", "kategory": "subdivision"}
   ]
 
